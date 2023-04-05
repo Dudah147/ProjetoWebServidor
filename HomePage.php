8c615daccff98dfd1b074618fbd40b029b7bb09e
Duda <mdudu2213@gmail.com>
2023-04-04 21:09:22 -0300

    feat: mudando para php

--- HomePage.php
+++ HomePage.php
@@ -15,30 +15,7 @@
 </head>
 <body style=" margin: 0; padding: 0; font-size: 16px;font-family: Verdana, Geneva, Tahoma, sans-serif; margin-left: 10;">
  
-    <header style="background-color: black; height: 100px;">
-  
-        <div style="display: flex; ">
-            <h1 style="color: aliceblue; align-items: flex-start; width: 850px;font-family: 'Norican', cursive;font-size: 42px;">
-                <a href="HomePage.html">&emsp;&emsp; PizzariaMMVRS</a>
-            </h1>
-
-            <div id="Header01" style=" justify-content: space-between; display: flex; flex-direction: row; ">
-            <a href="Cardapio.html" style="padding-right: 20px;">cardápio</a>
-            <a href="HomePage.html#sobre" style="padding-right: 20px;">sobre</a>
-            <a href="HomePage.html#promocoes" style="padding-right: 20px;">promoções</a>
-            <a href="HomePage.html#contato" style="padding-right: 20px;">contato</a>
-            <button style="width: 120px; height: 40px; float: right; margin-top: 50px; background-color: gold; border: none;" >
-                <a href="Pedido.html" style="color: black;">Pedidos Online</a>
-            </button>
-            <div id="container_img">
-                <img id="user" src="img/usuario.png">
-                <div style="display: flex; flex-direction: row; align-items:flex-end;">
-                    <img id="carrinho" src="img/carrinho.png">
-                    <span style="flex: block;color: black; background-color: white; border-radius: 2rem; width: 1rem; height: 1rem; text-align: center; font-size: .8rem; visibility: hidden;">1</span>
-                </div>
-            </div>
-        </div>
-    </header>
+    <?php echo "AAAAAAAAAA"; require("controllers/header.controller.php");?>
     
     <div id="container_home">
         <br>
@@ -165,7 +142,7 @@
         <div class="column">
             <div class="row">
                 <img src="img/inicio.png" class="imgs_user">
-                <a href="HomePage.html">Início</a>
+                <a href="HomePage.php">Início</a>
             </div>
             <div class="row">
                 <img src="img/endereco.png" class="imgs_user">
