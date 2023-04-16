8ecc8d3f0f4987d251892f44f973e96f6740b47c
Alexandre <alexandrerosascosta@gmail.com>
2023-04-16 15:20:05 -0300

    Atualização login

--- controllers/validar_login.controller.php
+++ controllers/validar_login.controller.php
@@ -1,21 +1,25 @@
 <?php 
     session_start();
 
-    $arquivo = "models/usuarios.model.json";
+    function validar_sessao(){
+        $arquivo = "models/usuarios.model.json";
 
-    if(isset($arquivo)){
-        fopen($arquivo, 'r');   
-        $json = json_decode(file_get_contents($arquivo), true);
-        for ($i=0; $i < count($json); $i++) { 
-            if($_POST['email'] == $json[$i]['email'] && $_POST['senha'] == $json[$i]['senha']){
-                $_SESSION['senha'] = $json[$i]['senha'];
-                $_SESSION['cpf'] = $json[$i]['cpf'];
+        if(isset($arquivo)){
+            fopen($arquivo, 'r');   
+            $json = json_decode(file_get_contents($arquivo), true);
+            for ($i=0; $i < count($json); $i++) { 
+                if($_POST['email'] == $json[$i]['email'] && $_POST['senha'] == $json[$i]['senha']){
+                    $_SESSION['senha'] = $json[$i]['senha'];
+                    $_SESSION['cpf'] = $json[$i]['cpf'];
+                    header("Location: pedido.php");
+                }
             }
+            
         }
-        header("Location: pedido.php");
     }
 
     if(isset($_GET['error'])){
         echo "Não foi possível validar a sessão";
     }
+    validar_sessao();
 ?>
--- pedido.php
+++ pedido.php
@@ -11,11 +11,11 @@
     <?php 
         require("controllers/header.controller.php");
 
-        session_start();/* 
+        session_start();
         if(!isset($_SESSION['cpf'])){
             header("Location: login.php?error=true");
             exit;
-        } */
+        }
         require("controllers/pedido.controller.php");
         require("controllers/carrinho.controller.php");
         require("controllers/usuario.controller.php");?>
