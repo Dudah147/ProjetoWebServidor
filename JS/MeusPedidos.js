adeae5144cceea4d100fe3efbf93efe5b7eda5ac
duda_note <duda_note>
2023-04-16 17:11:54 -0300

    feat: finalizado tela de meus_pedidos

--- JS/MeusPedidos.js
+++ JS/MeusPedidos.js
@@ -284,10 +284,53 @@ function descobrirCliente() {
 }
 
 //MEUS PEDIDOS
+const meus_pedidos = document.getElementById("meus_pedidos")
 const ver_detalhes = document.querySelectorAll(".ver_detalhes")
 console.log("Teste")
 for(el of ver_detalhes){
-    el.addEventListener( "click" , () =>{
-        window.el
-    })
+    el.addEventListener( "click" , ver_detalhes_event)
+}
+
+function ver_detalhes_event(){
+    for(pedido of pedidos){
+        if(pedido["id_pedido"] == this.parentNode.children[0].getAttribute("id_pedido")){
+            abre_pedido(pedido)
+        }
+    }
+}
+
+function abre_pedido(pedido){
+    meus_pedidos.style.display = "none"
+    const div_pedido = document.createElement("div")
+    div_pedido.setAttribute("class", "div_pedido")
+    container_meus_pedidos.appendChild(div_pedido)
+    
+    div_pedido.innerHTML = `
+        <button id="botao_voltar">Voltar</button>
+        <h2>Pedido ${pedido['id_pedido']}</h2>
+        <span>Realizado às: ${pedido['data']}</span>
+        <hr>
+        <h3>PRODUTOS</h3>
+    `
+    for(item of pedido["itens"]){
+        div_pedido.innerHTML += `
+            <div class="item">
+                <span>${item['quantidade']}un - ${item['tamanho']}</span>
+                <strong>R$ ${item['preco']}</strong>
+            </div>
+        `
+    }
+    div_pedido.innerHTML += `
+    <hr>
+        <span>Total do pedido: <strong style="color: green"> R$${pedido['valor_total']}</strong></span>
+    `
+
+    const botao_voltar = document.getElementById("botao_voltar")
+    botao_voltar.addEventListener("click", voltar_pedidos)
+}
+
+function voltar_pedidos(){
+    console.log(this.parentNode)
+    this.parentNode.remove()
+    meus_pedidos.style.display = "flex"
 }
--- controllers/verifica_pedidos.controller.php
+++ controllers/verifica_pedidos.controller.php
@@ -10,9 +10,10 @@
             $pedido_id = $pedido["id_pedido"];
             $pedido_data = $pedido["data"];
             $pedido_total = $pedido["valor_total"];
+            $win[] = $pedido;
             echo "<div class='pedido'>
                 <div>
-                    <h3>Pedido  $pedido_id </h3>
+                    <h3 id_pedido=$pedido_id>Pedido  $pedido_id </h3>
                     <span style='color:grey; font-size: .8rem'>$pedido_data</span>
                     <button class='ver_detalhes'>Ver detalhes</button>
                 </div>
@@ -21,11 +22,15 @@
                 </div>
             </div>";
             $verifica = true;
-            $win[] = $pedido;
+            
         }
     }
 
 
     if($verifica == false){
         echo "Você não possui pedidos";
+    }
+    else{
+        $win = json_encode($win);
+        echo "<script>let pedidos = $win</script>";
     }
