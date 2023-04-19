7f91bad4866d6f91a7beae2b578248783789111a
Alexandre Rosas Costa <alexandrerosascosta@gmail.com>
2023-04-18 21:25:18 -0300

    Deslogar definitivo

--- views/logado.view.php
+++ views/logado.view.php
@@ -1,13 +1,14 @@
 <div id="container">
 <span id="text_login">Login</span>
 <hr id="underline">
-
 <div id="login">
     <div class="cadastro">  
         <div class="inputgroup">
-                    <h1 id="boas-vindas" style="text-align: center;">Olá, <?php echo $_SESSION['nome'];?></h1>
-                    <h3 style="text-align: center;">Você já está logado!</h3>
-                    <button type="button" id="sair">Sair</button>
+            <h1 id="boas-vindas" style="text-align: center;">Olá, <?php echo $_SESSION['nome'];?></h1>
+            <h3 style="text-align: center;">Você já está logado!</h3>
+            <form action="controllers/deslogar.controller.php?action=deslogar" method="POST">
+                <button type="submit" id="sair">Sair</button>
+            </form>
         </div>
     </div>
 </div>
